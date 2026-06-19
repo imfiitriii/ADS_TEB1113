b92e377d44f7c2b5553f31627a8ae6df4933a1e4
Im <imranfitri06@gmail.com>
2026-06-19 08:43:48 +0800

    initial commit

---
+++ 2026_06_12-Task3/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct Node {
+    string data;
+    Node* next; //pointer variable, stores address
+};
+
+int main() {
+    // Node* head = new Node(); //head is pointer variable, stores a node address
+    // Node* second = new Node();
+    // Node* third = new Node();
+
+    // head->data = "Aimar";
+    // head->next = second;
+
+    // second->data = "Ahmad";
+    // second->next = third;
+
+    // third->data = "anjana";
+    // third->next = NULL;
+
+    // // Traverse and print
+    // Node* temp = head;
+
+    // while (temp != NULL) {
+    //     cout << temp->data << " ";
+    //     temp = temp->next;
+    // }
+
+    // return 0;
+
+    Node* node1 = new Node();
+    Node* node2 = new Node();
+    Node* node3 = new Node();
+
+    node1->data = "Aimar";
+    node1->next = node2;
+
+    node2->data = "Ahmad";
+    node2->next = node3;
+
+    node3->data = "anjana";
+    node3->next = NULL;
+
+    //TASK TODO : INSERT
+}
