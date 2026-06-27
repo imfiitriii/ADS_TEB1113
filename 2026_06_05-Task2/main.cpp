c45e0e40802964b1cf1f2352012b764a39000745
Im <imranfitri06@gmail.com>
2026-06-27 22:11:32 +0800

    completed task 2

--- 2026_06_05-Task2/main.cpp
+++ 2026_06_05-Task2/main.cpp
@@ -1,45 +1,137 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-struct Student{
-    string id;
-    string name;
-    string course;
-    int age;
-};
+void easy();
+void medium();
+void hard();
 
-int main() {
-    
-    Student student[5];
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Student " << i+1 << endl;
-        cout << "---------------------------" << endl;
-        cout << "Enter name : " ;
-        cin >> student[i].name;
-        cout << "Enter id : " ;
-        cin >> student[i].id;
-        cout << "Enter course : " ;
-        cin >> student[i].course;
-        cout << "Enter age : " ;
-        cin >> student[i].age;
-        cout << "---------------------------" << endl;
-    }
-    
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Student " << i+1 << endl;
-        cout << "---------------------------" << endl;
-        cout << "Name : " << student[i].name << endl;
-        cout << "ID : " << student[i].id << endl;
-        cout << "Course : " << student[i].course << endl;
-        cout << "Age : " << student[i].age << endl;
-        cout << "---------------------------" << endl;
-    }
-    
-
+int main()
+{
+    easy(); // CHANGE THE FUNCTION NAME TO easy(), medium(), hard()
     return 0;
+}
+
+// EASY QUESTION : SORT THE MATRIX
+void easy()
+{
+    int n;
+    cout << "Enter matrix size: ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+    vector<int> arr;
+
+    cout << "Enter matrix:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+            arr.push_back(matrix[i][j]);
+        }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int k = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i][j] = arr[k++];
+        }
+    }
+
+    cout << "\nSorted Matrix\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << matrix[i][j] << " ";
+
+        cout << endl;
+    }
+}
+
+// MEDIUM QUESTION : ROTATE THE MATRIX BY 180
+void medium()
+{
+    int n;
+
+    cout << "Enter matrix size: ";
+    cin >> n;
+
+    int matrix[100][100];
+    int result[100][100];
+
+    cout << "Enter matrix:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cin >> matrix[i][j];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[n - 1 - i][n - 1 - j] = matrix[i][j];
+        }
+    }
+
+    cout << "\nRotated Matrix\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << result[i][j] << " ";
+
+        cout << endl;
+    }
+}
+
+// HARD QUESTION : FIND PAIR IN MATRIX
+void hard()
+{
+    int n;
+
+    cout << "Enter matrix size: ";
+    cin >> n;
+
+    int matrix[100][100];
+
+    cout << "Enter matrix:\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cin >> matrix[i][j];
+    }
+
+    int maxDiff = INT_MIN;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            for (int k = i + 1; k < n; k++)
+            {
+                for (int l = j + 1; l < n; l++)
+                {
+                    if (matrix[k][l] - matrix[i][j] > maxDiff)
+                    {
+                        maxDiff = matrix[k][l] - matrix[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "Maximum Difference = " << maxDiff << endl;
 }
