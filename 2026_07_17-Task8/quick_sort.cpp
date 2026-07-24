16bb5c4bf16c9447b8a63819408c8bbb619e8620
Im <imranfitri06@gmail.com>
2026-07-24 11:34:21 +0800

    Create quick_sort.cpp

---
+++ 2026_07_17-Task8/quick_sort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Partition function
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];   // Last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Place pivot in its correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+// Quick Sort
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
